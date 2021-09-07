f1c7d31c16c5a255f2b87d89b1bc7dce39a670ab
Christie Koehler <ck@christi3k.net>
2021-09-07 13:52:12 -0700

    bit more stuff

---
+++ doc.go
@@ -0,0 +1,4 @@
+// copyright
+
+// Practice code from "Distributed Services with Go" by Travis Jeffrey, published by Pragmatic Bookshelf.
+package main
--- internal/server/log.go
+++ internal/server/log.go
@@ -14,7 +14,26 @@ func NewLog() *Log {
 	return &Log{}
 }
 
-func (c *Log) Apprend(record Record) (uinit64, error) {
+func (c *Log) Append(record Record) (uinit64, error) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
+	record.Offset = uint64(len(c.records))
+	c.records = append(c.records.record)
+	return record.Offset, nil
 }
+
+func (c *Log) Read(offset uint64) (Record, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if offset >= uint64(len(c.records)) {
+		return Record{}, ErrOffsetNotFound
+	}
+	return c.records[offset], nil
+}
+
+type Record struct {
+	Value  []byte `json:"value"`
+	Offset uint64 `jason:"offset"`
+}
+
+var ErrOffsetNotFound = fmt.Errorf("offset not found")
